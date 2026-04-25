cc4634662f32b4d043f64e504a1afdbdbc4f48b7
Victor <victorclifford29@gmail.com>
2026-04-25 16:15:57 +0100

    - docs fixes to serve with exxxpress

--- src/app.js
+++ src/app.js
@@ -35,14 +35,14 @@ app.use('/api/books', bookRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/orders', orderRoutes);
 
-// const path = require('path');
+const path = require('path');
 
 // Serve static files from the 'public' directory
-// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Basic health check route
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
